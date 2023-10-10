26e952154fcf68ddf444270fdb7ebbc51f58877a
Amirhossein Rezapanah <a.rezapanah@dotin.ir>
2023-10-10 14:36:27 +0330

    refactor: new project structure implemented
    atomic desighn and git flow

---
+++ src/shared/libs/images.ts
@@ -0,0 +1,12 @@
+const BASE_IMAGE_URL = "/assets/images";
+
+const images = {
+  logo: {
+    next: { src: `${BASE_IMAGE_URL}/next.svg`, alt: "next" },
+    vercel: { src: `${BASE_IMAGE_URL}/vercel.svg`, alt: "vercel" },
+  },
+  img: {},
+  icons: {},
+};
+
+export default images;
