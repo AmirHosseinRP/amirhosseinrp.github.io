ff45368294e7b571ad1ba2add4b8e49a6fdb475c
AmirHosseinRP <rezapanahhf1@gmail.com>
2024-09-17 16:09:25 +0330

    feat: jext config rolled back

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,24 @@ import withMDX from "@next/mdx";
 import withPWA from "next-pwa";
 
 const withPWAConfig = withPWA({
+  disable: process.env.NODE_ENV !== "production",
   dest: "public",
   register: true,
   skipWaiting: true,
 });
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
+    serviceWorker: true,
+    webpackBuildWorker: true,
+    isrMemoryCacheSize: 0,
     mdxRs: true,
   },
   httpAgentOptions: {
     keepAlive: true,
   },
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
 
 export default withPWAConfig(withMDX(nextConfig));
