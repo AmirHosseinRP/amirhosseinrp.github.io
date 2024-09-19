80d917bae47c7dab566b0f3e9e3c4e612f32899a
AmirHosseinRP <rezapanahhf1@gmail.com>
2024-09-19 10:43:00 +0330

    feat: config removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,31 @@
-import withMDX from "@next/mdx";
-import withPWA from "next-pwa";
+// import withMDX from "@next/mdx";
+// import withPWA from "next-pwa";
 
-const withPWAConfig = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
+// const withPWAConfig = withPWA({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+// });
 
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  trailingSlash: true,
-  reactStrictMode: true,
-  experimental: {
-    serviceWorker: true,
-    webpackBuildWorker: true,
-    isrMemoryCacheSize: 0,
-    mdxRs: true,
-  },
-  httpAgentOptions: {
-    keepAlive: true,
-  },
-};
+// /** @type {import("next").NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   trailingSlash: true,
+//   reactStrictMode: true,
+//   experimental: {
+//     serviceWorker: true,
+//     webpackBuildWorker: true,
+//     isrMemoryCacheSize: 0,
+//     mdxRs: true,
+//   },
+//   httpAgentOptions: {
+//     keepAlive: true,
+//   },
+// };
 
-export default withPWAConfig(withMDX(nextConfig));
+// export default withPWAConfig(withMDX(nextConfig));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
