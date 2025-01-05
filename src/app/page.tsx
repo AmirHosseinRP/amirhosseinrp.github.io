de0fdc0fc146fd1afddd28c7e6f16dea9f91b4dc
Amirhossein Rezapanah <a.rezapanah@dotin.ir>
2025-01-05 17:30:03 +0330

    feat: components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,13 @@ export default function Home() {
     <>
       <Spotlight
         primaryContent={
-          <div className="text-6xl text-center leading-none font-bold w-96 h-96 bg-red-600">primary content here</div>
+          <div className="text-6xl text-center leading-none font-bold w-96 h-96">primary content here</div>
         }
         hoverContent={
-          <div className="text-6xl text-center leading-none font-bold text-black w-96 h-96 bg-blue-600">
-            hover content here
-          </div>
+          <div className="text-6xl text-center leading-none font-bold text-black w-96 h-96">hover content here</div>
         }
       />
+
       {/* <Eye /> */}
     </>
   );
--- src/components/atoms/Spotlight/Spotlight.tsx
+++ src/components/atoms/Spotlight/Spotlight.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { type ReactNode, useEffect, useRef } from "react";
+import CursorMotion from "../CursorMotion/CursorMotion";
 
 interface CursorPosition {
   x: number;
@@ -12,7 +13,7 @@ interface Props {
   hoverContent: ReactNode;
 }
 
-const HoverReveal = (props: Props) => {
+const Spotlight = (props: Props) => {
   const hoveredRef = useRef<boolean>(false);
   const clipPathElement = useRef<HTMLDivElement | null>(null);
   const animationFrameId = useRef<number | null>(null);
@@ -22,6 +23,7 @@ const HoverReveal = (props: Props) => {
   const showHidden = (): void => {
     if (!hoveredRef.current) {
       hoveredRef.current = true;
+
       if (cursorCircleRef.current) {
         cursorCircleRef.current.style.width = "300px";
         cursorCircleRef.current.style.height = "300px";
@@ -32,10 +34,10 @@ const HoverReveal = (props: Props) => {
   const hideText = (): void => {
     if (hoveredRef.current) {
       hoveredRef.current = false;
+
       if (cursorCircleRef.current) {
-        cursorCircleRef.current.style.border = "none";
-        cursorCircleRef.current.style.width = "20px";
-        cursorCircleRef.current.style.height = "20px";
+        cursorCircleRef.current.style.width = "0";
+        cursorCircleRef.current.style.height = "0";
       }
     }
   };
@@ -72,27 +74,19 @@ const HoverReveal = (props: Props) => {
   }, []);
 
   return (
-    <div className="relative w-fit h-fit flex justify-center items-center">
-      {/* <div
-        ref={cursorCircleRef}
-        className="fixed pointer-events-none transition-all bg-green-400 rounded-full h-5 w-5 z-10"
-        style={{
-          transform: "translate(-50%, -50%)",
-          top: "0",
-          left: "0",
-        }}
-      /> */}
-
+    <div className="relative w-fit h-fit flex justify-center items-center z-40">
       <div onMouseOver={showHidden} onMouseOut={hideText} className="absolute inset-0">
         {props.primaryContent}
       </div>
 
+      <CursorMotion duration={0}>
+        <div ref={cursorCircleRef} className="bg-[#3990E2] w-0 h-0 rounded-full transition-all duration-300" />
+      </CursorMotion>
+
       <div
         ref={clipPathElement}
-        className="absolute inset-0"
+        className="absolute inset-0 pointer-events-none"
         style={{
-          transform: "translateX(-50%)",
-          pointerEvents: "none",
           clipPath: "circle(0px)",
         }}
       >
@@ -102,4 +96,4 @@ const HoverReveal = (props: Props) => {
   );
 };
 
-export default HoverReveal;
+export default Spotlight;
