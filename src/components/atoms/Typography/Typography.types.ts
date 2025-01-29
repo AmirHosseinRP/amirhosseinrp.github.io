79dcbfab5b0f9e98645db3d37ad72aea36011f81
Amirhossein Rezapanah <a.rezapanah@dotin.ir>
2025-01-29 16:32:52 +0330

    fix: lint errors fixed

--- src/components/atoms/Typography/Typography.types.ts
+++ src/components/atoms/Typography/Typography.types.ts
@@ -1,3 +1,35 @@
-export type TypographyVariant = "h1" | "h2" | "h3" | "h4" | "h5" | "title1" | "title2" | "title3" | "body1" | "body2" | "body3" | "label1" | "label2" | "label3" | "label4" | "caption1" | "caption2" | "btn-sm" | "btn-md" | "btn-lg";
+export type TypographyVariant =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "title1"
+  | "title2"
+  | "title3"
+  | "body1"
+  | "body2"
+  | "body3"
+  | "label1"
+  | "label2"
+  | "label3"
+  | "label4"
+  | "caption1"
+  | "caption2"
+  | "btn-sm"
+  | "btn-md"
+  | "btn-lg";
 
-export type TypographyColor = "prose-primary" | "prose-secondary" | "prose-hint" | "prose-inverse" | "prose-disabled" | "prose-link" | "prose-white" | "brand" | "info" | "success" | "warning" | "critical";
+export type TypographyColor =
+  | "prose-primary"
+  | "prose-secondary"
+  | "prose-hint"
+  | "prose-inverse"
+  | "prose-disabled"
+  | "prose-link"
+  | "prose-white"
+  | "brand"
+  | "info"
+  | "success"
+  | "warning"
+  | "critical";
