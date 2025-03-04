a72bdddcd6365021108ccc348717406951513f7a
Amirhossein Rezapanah <a.rezapanah@dotin.ir>
2025-03-04 09:33:17 +0330

    feat: env files implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
 import Container from "~/components/atoms/Container/Container";
 import Header from "~/components/atoms/Header/Header";
+import env from "~/shared/config/env";
 
 export default function Home() {
   return (
     <>
       <Header title="About me" />
-
+      <Link href={env.links.resume ?? ""} download>
+        download
+      </Link>
       <Container>Hello, My name is Amirhossein. I&apos;m a Frontend developer.</Container>
     </>
   );
---
+++ src/shared/config/env.ts
@@ -0,0 +1,7 @@
+const env = {
+  links: {
+    resume: process.env.NEXT_PUBLIC_RESUME_LINK,
+  },
+};
+
+export default env;
