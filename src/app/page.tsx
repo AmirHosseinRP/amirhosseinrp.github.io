9fbbcde86ba6fc94628d6c41c8654dc0a760e41c
Amirhossein Rezapanah <a.rezapanah@dotin.ir>
2025-03-11 14:07:28 +0330

    style: bottom hover implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
-import Button from "~/components/atoms/Button/Button";
 import Container from "~/components/atoms/Container/Container";
 import Header from "~/components/atoms/Header/Header";
-import Typography from "~/components/atoms/Typography/Typography";
 
 export default function Home() {
   return (
     <>
       <Header title="About me" />
 
-      <Container>
-        {/* Hello, My name is Amirhossein. I&apos;m a Frontend developer. */}
-        <div className="flex">
-          <Typography>a:</Typography>
-          <Button>Tab 02</Button>
-        </div>
-      </Container>
+      <Container>Hello, My name is Amirhossein. I&apos;m a Frontend developer.</Container>
     </>
   );
 }
